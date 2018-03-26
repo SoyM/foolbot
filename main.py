06b97c8b70d81a12e3c3c4c00182a532480b83b0
SoyM <huangxinjiang1996@gmail.com>
2018-03-26 22:50:10 +0800

    loop

--- main.py
+++ main.py
@@ -83,13 +83,14 @@ def setup():
 
 
 def get_set_mode():
-    r = requests.get('http://111.230.224.190/get_set_status/')
-    global ch
-    ch = r.json()['set_mode']
-    payload = {'bot_mode': ch}
-    r = requests.post('http://111.230.224.190/update_bot_motion/', json=payload)
-    print(r.text)
-    time.sleep(1)
+    while True:
+        r = requests.get('http://111.230.224.190/get_set_status/')
+        global ch
+        ch = r.json()['set_mode']
+        payload = {'bot_mode': ch}
+        r = requests.post('http://111.230.224.190/update_bot_motion/', json=payload)
+        print(r.text)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
